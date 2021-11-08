45001b849357668fa4e76f701ccd2296bc2f01d9
shizi <lan.zhu.123@163.com>
2021-11-08 10:07:51 +0800

    add 添加map

--- src/main/kotlin/com/simonalong/mikilin/MkValidators.kt
+++ src/main/kotlin/com/simonalong/mikilin/MkValidators.kt
@@ -1,8 +1,10 @@
 package com.simonalong.mikilin
 
 import com.simonalong.mikilin.util.ClassUtil
+import com.simonalong.mikilin.util.ClassUtil.isCheckedType
 import com.simonalong.mikilin.util.ObjectUtil
 import java.lang.reflect.Field
+import kotlin.reflect.KClass
 
 /**
  * @author     shizi
@@ -11,30 +13,22 @@ import java.lang.reflect.Field
 object MkValidators {
 
     fun check(value: Any?): Boolean {
-        return check(DEFAULT_GROUP, value)
-    }
+        if (null == value) {
+            return true
+        }
 
-    fun check(group: String?, value: Any?): Boolean {
         if (ObjectUtil.isEmpty(value)) {
             return true
         }
-        val groupDelegate = if (null == group || "" == group) DEFAULT_GROUP else group
 
-        // 待核查类型不核查，直接返回核查成功
-        return if (ClassUtil.isCheckedType(value.javaClass)) {
-            true
-        } else {
-            check(groupDelegate, value, ClassUtil.allFieldsOfClass(ClassUtil.peel(value)), MkValidators.getObjFieldMap(value), MkValidators.getWhiteMap(), MkValidators.getBlackMap())
+        // 基本类型
+        if (ClassUtil.isCheckedType(value::class)) {
+            return true
         }
-    }
 
-    private fun check(group: String, value: Any?, fieldSet: Set<Field>, objectFieldMap: Map<String, Set<String>>, whiteSet: Map<String, MatchManager>,
-                      blackSet: Map<String, MatchManager>): Boolean {
-        MkValidators.delegate.setParameter(group, value)
-        return try {
-            MkValidators.delegate.available(null, value, fieldSet, objectFieldMap, whiteSet, blackSet)
-        } finally {
-            MkValidators.delegate.clear()
-        }
+        // 搜集match
+
+        var matcherMap = MutableMap
+
     }
 }
